90c2d8960adb08b018326953903b9f3e9e9cdcfe
Rajat Jain <rajat.jain@metacube.com>
2019-07-26 11:48:53 +0530

    Sample profile app

---
+++ Sample To-do App/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var toDoController = require('./controller/todoController')
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+console.log(__dirname+ '/assets')
+app.use(express.static(__dirname + '/assets'));
+app.get('/',function(req, res) {
+	res.send('HomePage')	
+})
+
+app.get('/home',function(req, res) {
+	res.send('HomePage')	
+})
+app.get('/contact',function(req, res) {
+	res.render('profile', {name : 'to contact page.This page is not yet build'})	
+})
+app.get('/contact/:id',function(req, res) {
+	console.log(req.params.id)
+	res.render('profile', {name : req.params.id})	
+})
+
+toDoController(app);
+app.listen(3019);
